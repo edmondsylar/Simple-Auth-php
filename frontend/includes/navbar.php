9107a73f9391bd1be99390fa670ce70e70b7f32b
edmondsylar <edmonmusiitwa@gmail.com>
2022-07-13 21:50:24 +0300

    added login and registration pages

--- frontend/includes/navbar.php
+++ frontend/includes/navbar.php
@@ -8,7 +8,6 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="login.php">Login</a></li>
             <li><a href="register.php">Register</a></li>
-            <li><a href="logout.php">Logout</a></li>
         </ul>
     </div>
 </nav> 
--- frontend/register.php
+++ frontend/register.php
@@ -5,6 +5,27 @@
         <h2>Register </h2>
         <!-- include navbar here -->
         <?php include "./includes/navbar.php"; ?>
+        
+        <!-- registration form -->
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <form action="../backend/register.php" method="post">
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Confirm Password</label>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password" placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+
     </div>
 
 <?php include "./includes/footer.php"; ?>
