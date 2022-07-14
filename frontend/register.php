c0f7deda017e17d06f7a4d1c367442579075e165
clive <96369706+CliveMcEvadeen@users.noreply.github.com>
2022-07-14 22:22:26 +0300

    Update register.php

--- frontend/register.php
+++ frontend/register.php
@@ -9,14 +9,18 @@
         <!-- registration form -->
         <div class="row">
             <div class="col-md-6 col-md-offset-3">
-                <form action="../backend/register.php" method="post">
+                <form action="../backend/register.php" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="name">Full name</label>
+                        <input type="text" id="name" placeholder="Name" name="name" required="required" spellcheck="true">
+                    </div>
                     <div class="form-group">
                         <label for="email">Email</label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="Email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" required="required">
                     </div>
                     <div class="form-group">
                         <label for="password">Confirm Password</label>
