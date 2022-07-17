922d8730da3905271eb43e215d24fa3f7bdffd59
clive <clivekakeeto47@gmail.com>
2022-07-17 18:06:38 +0300

    modified backend.php

--- backend/backend.php
+++ backend/backend.php
@@ -18,14 +18,18 @@
         // function takes in a parameter called $email and $password.
 
         public function login($email, $password){
-            // select all from the users table where the email is equal to the email parameter and the password is equal to the password parameter.
+            
+            if (preg_match("0-9A-za-z{!@#%$&^*()_-}[]'?/`~,.",$password) && preg_filter($email,FILTER_VALIDATE_EMAIL)){
+            // select all from the users table where the email is equal to the email parameter and             //The password is equal to the password parameter.
             $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
             $conn = $connection->prepare($sql);
             $conn->execute();
             $result = $conn->fetchAll();
             // if the result is greater than 0.
             echo $result;
-            
+            }
+            else{echo "Invalid input: check the details in the filed";
+            }
             // get connection from connect.php.
             return 'login';
         }
